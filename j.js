7237cafab8696f45dbf56b13ecbe71e2ec9fb4e6
Harpreet Tohra <harpreet.s@chitkara.edu.in>
2020-07-24 22:16:27 +0530

    commit

--- j.js
+++ j.js
@@ -1,7 +1,2 @@
 var msg = 'Hello World';
 console.log(msg);
-response.writeHead(301, {
-  Location: "http" + (request.socket.encrypted ? "s" : "") + "://" + 
-    request.headers.host + newRoom
-});
-response.end();
